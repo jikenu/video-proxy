5df8458a66d90bbdb7dd56c0badab74f276e999a
jikenu <2961819202@qq.com>
2024-06-18 18:17:40 +0800

    change method name

--- _worker.js
+++ _worker.js
@@ -1,11 +1,122 @@
-export default {
-  async fetch(request, env) {
-    let url = new URL(request.url);
-    if (url.pathname.startsWith('/')) {
-      url.hostname = 'hanime1.me'
-      let new_request = new Request(url, request);
-      return fetch(new_request);
+// 反代目标网站ipv6.google.com.hk
+const upstream = 'hanime1.me'
+const upstream_v4 = 'hanime1.me'
+//www.google.com.hk
+// 访问区域黑名单（按需设置）.
+const blocked_region = ['TK']
+
+//资源重定向
+const replace_dict = {
+  $upstream: '$custom_domain',
+  'hanime1.me/': '/', //填入你的子域名
+//  'vdownload.hembed.com': 'video.mycf2hj.filegear-sg.me'
+}
+
+addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request))
+})
+
+async function fetch(request) {
+  const region = request.headers.get('cf-ipcountry').toUpperCase()
+//   const ip_address = request.headers.get('cf-connecting-ip')
+//   const user_agent = request.headers.get('user-agent')
+
+  let response = null
+  let url = new URL(request.url)
+  let url_host = url.host
+
+  if (url.protocol == 'http:') {
+    url.protocol = 'https:'
+    response = Response.redirect(url.href)
+    return response
+  }
+
+  //检查是否为图片搜索
+  var key = url.href
+  var ikey1 = 'tbm=isch'
+  var ikey2 = '/img'
+  if ((key.search(ikey1) == -1) && (key.search(ikey2) == -1)) {
+    var upstream_domain = upstream
+  } else {
+    var upstream_domain = upstream_v4
+  }
+
+  url.host = upstream_domain
+
+  if (blocked_region.includes(region)) {
+    response = new Response(
+      'Access denied: WorkersProxy is not available in your region yet.',
+      {
+        status: 403,
+      }
+    )
+  } else {
+    let method = request.method
+    let request_headers = request.headers
+    let new_request_headers = new Headers(request_headers)
+
+    new_request_headers.set('Host', upstream_domain)
+    new_request_headers.set('Referer', url.href)
+
+    let original_response = await fetch(url.href, {
+      method: method,
+      headers: new_request_headers,
+    })
+
+    let original_response_clone = original_response.clone()
+    let original_text = null
+    let response_headers = original_response.headers
+    let new_response_headers = new Headers(response_headers)
+    let status = original_response.status
+
+    new_response_headers.set('cache-control', 'public, max-age=14400')
+    new_response_headers.set('access-control-allow-origin', '*')
+    new_response_headers.set('access-control-allow-credentials', true)
+    new_response_headers.delete('content-security-policy')
+    new_response_headers.delete('content-security-policy-report-only')
+    new_response_headers.delete('clear-site-data')
+
+    const content_type = new_response_headers.get('content-type')
+    if (content_type.includes('text/html') && content_type.includes('UTF-8')) {
+      // && content_type.includes('UTF-8')
+      original_text = await replace_response_text(
+        original_response_clone,
+        upstream_domain,
+        url_host
+      )
+    } else {
+      original_text = original_response_clone.body
     }
-    return env.ASSETS.fetch(request);
-  },
-};
+
+    response = new Response(original_text, {
+      status,
+      headers: new_response_headers,
+    })
+  }
+  return response
+}
+
+async function replace_response_text(response, upstream_domain, host_name) {
+  let text = await response.text()
+
+  var i, j
+  for (i in replace_dict) {
+    j = replace_dict[i]
+    if (i == '$upstream') {
+      i = upstream_domain
+    } else if (i == '$custom_domain') {
+      i = host_name
+    }
+
+    if (j == '$upstream') {
+      j = upstream_domain
+    } else if (j == '$custom_domain') {
+      j = host_name
+    }
+
+    let re = new RegExp(i, 'g')
+    text = text.replace(re, j)
+  }
+
+  return text
+}
